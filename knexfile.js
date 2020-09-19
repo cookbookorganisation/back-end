3abb834072eb75ac54f3f234122f61e93676ce34
seanaleid <seanaleid@gmail.com>
2020-09-19 14:08:01 -0400

    Change production enviornment to pg

--- knexfile.js
+++ knexfile.js
@@ -1,36 +1,31 @@
 // Update with your config settings.
 
 module.exports = {
-
-    development: {
-        client: 'sqlite3',
-        connection: {
-        filename: './data/dev.sqlite3'
-        }, 
-        migrations: {
-            directory: './data/migrations'
-        },
-        useNullAsDefault: true,
-        pool: {
-            afterCreate: (conn, done) => {
-            // runs after a connection is made to the sqlite engine
-            conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-            },
-        },
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/dev.sqlite3",
     },
-    production: {
-        client: 'postgresql',
-        connection: {
-        database: 'my_db',
-        user:     'username',
-        password: 'password'
-        },
-        pool: {
-        min: 2,
-        max: 10
-        },
-        migrations: {
-        tableName: 'knex_migrations'
-        }
-    }
+    migrations: {
+      directory: "./data/migrations",
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      },
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
